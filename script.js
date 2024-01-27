add25cac6cee3d2e2e94351e6c48dc3a5f37f588
miayamamoto <157864960+miayamamoto@users.noreply.github.com>
2024-01-26 19:48:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// ChatGPT helped //
+const instagramButton = document.getElementById('instagramButton');
+
+function goToInstagramPage() {
+    const instagramPageURL = 'https://www.instagram.com/monoyoop/';
+
+    window.open(instagramPageURL, '_blank');
+}
+
+instagramButton.addEventListener('click', goToInstagramPage);
+// This Section as well //
